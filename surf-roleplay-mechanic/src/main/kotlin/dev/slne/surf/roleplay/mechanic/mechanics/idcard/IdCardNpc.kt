586d02cb42fcc354cdd854e2431fdad5cc24b444
MikeyLLP <167801877+MikeyLLP@users.noreply.github.com>
2025-08-04 14:20:46 +0200

    feat: The pig is now an NPC.

---
+++ surf-roleplay-mechanic/src/main/kotlin/dev/slne/surf/roleplay/mechanic/mechanics/idcard/IdCardNpc.kt
@@ -0,0 +1,27 @@
+package dev.slne.surf.roleplay.mechanic.mechanics.idcard
+
+import dev.slne.surf.npc.api.dsl.npc
+import dev.slne.surf.npc.api.surfNpcApi
+import dev.slne.surf.roleplay.mechanic.mechanics.idcard.IdCardMechanicImpl.NPC_NAME
+
+object IdCardNpc {
+    suspend fun spawnNpc() {
+        val npcSkin = surfNpcApi.getSkin("MikeyLLP")
+
+        npc {
+            uniqueName = NPC_NAME
+            displayName = {
+                primary("Bürgeramt")
+            }
+
+            skin = npcSkin
+
+            location {
+                world = "world"
+                x = 2.0
+                y = 100.0
+                z = 0.0
+            }
+        }
+    }
+}
