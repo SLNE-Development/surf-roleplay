5823e83582f8f87fa00631eb9fe6a948d024cb36
Ammo <ammo@slne.dev>
2025-08-08 20:28:34 +0200

    Add balanceTypeArgument for command API: implement custom argument for balance type selection

--- surf-roleplay-paper/src/main/kotlin/dev/slne/surf/roleplay/paper/SurfRoleplay.kt
+++ surf-roleplay-paper/src/main/kotlin/dev/slne/surf/roleplay/paper/SurfRoleplay.kt
@@ -2,15 +2,17 @@ package dev.slne.surf.roleplay.paper
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
 import com.github.shynixn.mccoroutine.folia.launch
-import dev.jorel.commandapi.kotlindsl.commandAPICommand
-import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.jorel.commandapi.kotlindsl.*
 import dev.slne.surf.roleplay.api.player.rpPlayer
+import dev.slne.surf.roleplay.api.player.utils.BalanceType
 import dev.slne.surf.roleplay.core.RpCore
 import dev.slne.surf.roleplay.core.RpDatabase
 import dev.slne.surf.roleplay.core.player.license.licenseServiceImpl
 import dev.slne.surf.roleplay.mechanic.mechanicRegistryImpl
+import dev.slne.surf.roleplay.paper.command.args.balanceTypeArgument
 import dev.slne.surf.roleplay.paper.listeners.ListenerManager
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 val plugin get() = JavaPlugin.getPlugin(SurfRoleplay::class.java)
@@ -40,6 +42,117 @@ class SurfRoleplay : SuspendingJavaPlugin() {
         mechanicRegistryImpl.registerBukkitHandlers()
 
         licenseServiceImpl.onEnable()
+
+        commandAPICommand("balance") {
+            subcommand("get") {
+                balanceTypeArgument("balanceType")
+                entitySelectorArgumentOnePlayer("player")
+
+                playerExecutor { sender, args ->
+                    val player: Player by args
+                    val balanceType: BalanceType by args
+
+                    plugin.launch {
+                        val rpPlayer = player.rpPlayer()
+                        val balance = rpPlayer.getBalance(balanceType)
+
+                        sender.sendText {
+                            appendPrefix()
+
+                            info("Der Kontostand von ")
+                            append(rpPlayer)
+                            info(" beträgt: ")
+                            variableValue(balance)
+                            info(".")
+                        }
+                    }
+                }
+            }
+
+            subcommand("set") {
+                balanceTypeArgument("balanceType")
+                entitySelectorArgumentOnePlayer("player")
+                integerArgument("amount")
+
+                playerExecutor { sender, args ->
+                    val player: Player by args
+                    val balanceType: BalanceType by args
+                    val amount: Int by args
+
+                    plugin.launch {
+                        val rpPlayer = player.rpPlayer()
+                        val currentAmount = rpPlayer.getBalance(balanceType)
+
+                        rpPlayer.removeBalance(balanceType, currentAmount)
+                        rpPlayer.addBalance(balanceType, amount)
+
+                        sender.sendText {
+                            appendPrefix()
+
+                            info("Der Kontostand von ")
+                            append(rpPlayer)
+                            info(" wurde auf ")
+                            variableValue(amount)
+                            info(" gesetzt.")
+                        }
+                    }
+                }
+            }
+
+            subcommand("add") {
+                balanceTypeArgument("balanceType")
+                entitySelectorArgumentOnePlayer("player")
+                integerArgument("amount")
+
+                playerExecutor { sender, args ->
+                    val player: Player by args
+                    val balanceType: BalanceType by args
+                    val amount: Int by args
+
+                    plugin.launch {
+                        val rpPlayer = player.rpPlayer()
+                        rpPlayer.addBalance(balanceType, amount)
+
+                        sender.sendText {
+                            appendPrefix()
+
+                            info("Der Kontostand von ")
+                            append(rpPlayer)
+                            info(" wurde um ")
+                            variableValue(amount)
+                            info(" erhöht.")
+                        }
+                    }
+                }
+            }
+
+            subcommand("remove") {
+                balanceTypeArgument("balanceType")
+                entitySelectorArgumentOnePlayer("player")
+                integerArgument("amount")
+
+                playerExecutor { sender, args ->
+                    val player: Player by args
+                    val balanceType: BalanceType by args
+                    val amount: Int by args
+
+                    plugin.launch {
+                        val rpPlayer = player.rpPlayer()
+                        rpPlayer.removeBalance(balanceType, amount)
+
+                        sender.sendText {
+                            appendPrefix()
+
+                            info("Der Kontostand von ")
+                            append(rpPlayer)
+                            info(" wurde um ")
+                            variableValue(amount)
+                            info(" verringert.")
+                        }
+                    }
+                }
+            }
+        }
 
         commandAPICommand("active-identity") {
             playerExecutor { player, args ->
