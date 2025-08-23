cb9ab4e409b95708295aca8d17cd86103554b78f
twisti <denizgebauer@web.de>
2025-08-23 10:35:49 +0200

    Remove `PaperRpInstance` and refactor `RpInstance` to simplify lifecycle handling.

--- surf-roleplay-core/surf-roleplay-core-common/src/main/kotlin/dev/slne/surf/roleplay/core/common/RpInstance.kt
+++ surf-roleplay-core/surf-roleplay-core-common/src/main/kotlin/dev/slne/surf/roleplay/core/common/RpInstance.kt
@@ -2,35 +2,24 @@ package dev.slne.surf.roleplay.core.common
 
 import dev.slne.surf.cloud.api.common.util.forEachAnnotationOrdered
 import dev.slne.surf.cloud.api.common.util.forEachAnnotationOrderedReversed
-import dev.slne.surf.roleplay.core.common.mechanics.MechanicRegistry
 import org.springframework.beans.factory.ObjectProvider
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import javax.annotation.OverridingMethodsMustInvokeSuper
 
 @Component
 class RpInstance(private val lifecycles: ObjectProvider<RpLifecycle>) {
-    @Autowired
-    private lateinit var mechanicRegistry: MechanicRegistry
-
 
     suspend fun onLoad() {
         lifecycles.forEachAnnotationOrdered { it.onLoad() }
-
-        mechanicRegistry.loadMechanics()
     }
 
     suspend fun onEnable() {
         lifecycles.forEachAnnotationOrdered { it.onEnable() }
-
-        mechanicRegistry.enableMechanics()
     }
 
     @OverridingMethodsMustInvokeSuper
     suspend fun onDisable() {
         lifecycles.forEachAnnotationOrderedReversed { it.onDisable() }
-
-        mechanicRegistry.disableMechanics()
     }
 
 }
